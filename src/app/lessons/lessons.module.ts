001149b28606bb0c0195c84a4c1e0e0bd715d081
njeruedwin <edwinnjeru77@gmail.com>
2022-12-01 17:12:56 +0300

    implemented the custom pipe :artist:

--- src/app/lessons/lessons.module.ts
+++ src/app/lessons/lessons.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CustomPipeComponent } from './pages';
 import { LessonsComponent } from './pages';
+import { FormsModule } from '@angular/forms';
 import { LessonsRoutingModule } from './lessons-routing.module';
+import { TemparetureConverterPipe } from '../core';
 
 @NgModule({
-  imports: [LessonsRoutingModule],
-  declarations: [LessonsComponent, CustomPipeComponent],
+  imports: [LessonsRoutingModule, FormsModule],
+  declarations: [
+    LessonsComponent,
+    CustomPipeComponent,
+    TemparetureConverterPipe,
+  ],
   exports: [],
 })
 export class LessonsModule {}
--- src/app/lessons/pages/custompipe/custompipe.component.ts
+++ src/app/lessons/pages/custompipe/custompipe.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'custom-pipe',
   templateUrl: './custompipe.component.html',
 })
-export class CustomPipeComponent {}
+export class CustomPipeComponent {
+  title: string = 'This is the custom pipe component';
+  subtitle: string =
+    'In this lesson created a pipe that tranforms digits from Fahrenheit to celcius and vice versa';
+  celcius: number;
+  fahrenheit: number;
+}
